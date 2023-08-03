de07bdf0ec3fd2005990dd62405785d742b9693c
anuuz1307 <memaneanuja@gmail.com>
2023-08-04 01:44:47 +0530

    first commit

---
+++ database/db.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+
+//dotenv.config();
+
+const DBConnection = async () => {
+ 
+
+    const MONGO_URI = `mongodb://anujam:1307@ac-cknogj1-shard-00-00.owzddlt.mongodb.net:27017,ac-cknogj1-shard-00-01.owzddlt.mongodb.net:27017,ac-cknogj1-shard-00-02.owzddlt.mongodb.net:27017/?ssl=true&replicaSet=atlas-fi9od2-shard-0&authSource=admin&retryWrites=true&w=majority`;
+    try {
+        await mongoose.connect(MONGO_URI, { useNewUrlParser: true });
+        console.log('Database connected successfully');
+    } catch (error) {
+        console.log('Error while connecting with the database ', error.message);
+    }
+}
+
+export default DBConnection;
